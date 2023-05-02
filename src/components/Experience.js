1676ee200cbec8a837a931af636a07de287f4d54
ftrbnd <giosalas25@gmail.com>
2023-05-02 14:51:16 -0700

    Start on Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 
 class Experience extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
+
+        this.state = {
+            experience: {
+                title: '',
+                tools: [],
+                tasks: [],
+            },
+            allExperience: [],
+            isReadOnly: true
+        };
+        
+        this.buttonClick = this.buttonClick.bind(this);
+    }
+
+    buttonClick() {
+        this.setState({
+            isReadOnly: !this.state.isReadOnly
+        });
     }
 
     render() {
+        const { isReadOnly } = this.state;
+
         return (
-            <div>
+            <div className="experienceInfo">
+                <h2>Experience</h2>
                 
+                <button
+                    onClick={this.buttonClick}
+                    type="submit"
+                >
+                    {isReadOnly ? 'Edit' : 'Done'}
+                </button>
             </div>
-        )
+        );
     }
 }
 
